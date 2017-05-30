7d26d3e4f73e9a5ed9962d18b445a7ce473b414d
Geoffrey Biggs <git@killbots.net>
2017-05-30 16:25:31 +0900

    Specified pose example

--- src/pick_and_placer.cpp
+++ src/pick_and_placer.cpp
@@ -12,7 +12,24 @@ int main(int argc, char **argv) {
 
   moveit::planning_interface::MoveGroupInterface arm("arm");
   arm.setPoseReferenceFrame("base_link");
-  arm.move();
+
+  // Prepare
+  ROS_INFO("Moving to prepare pose");
+  geometry_msgs::PoseStamped pose;
+  pose.header.frame_id = "base_link";
+  pose.pose.position.x = 0.2;
+  pose.pose.position.y = 0.0;
+  pose.pose.position.z = 0.1;
+  pose.pose.orientation.x = 0.0;
+  pose.pose.orientation.y = 0.707106;
+  pose.pose.orientation.z = 0.0;
+  pose.pose.orientation.w = 0.707106;
+
+  arm.setPoseTarget(pose);
+  if (!arm.move()) {
+    ROS_WARN("Could not move to prepare pose");
+    return 1;
+  }
 
   ros::shutdown();
   return 0;
